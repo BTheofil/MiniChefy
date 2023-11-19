788cbc5aa5c7e1b41f70118b5c17e58bde5702ef
BTheofil <btheofil7@gmail.com>
2023-11-19 15:02:31 +0100

    add preview

--- app/src/main/java/hu/tb/minichefy/presentation/screens/recipe_details/RecipeDeatilsScreen.kt
+++ app/src/main/java/hu/tb/minichefy/presentation/screens/recipe_details/RecipeDeatilsScreen.kt
@@ -7,8 +7,10 @@ import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.hilt.navigation.compose.hiltViewModel
+import hu.tb.minichefy.domain.model.Recipe
 
 @Composable
 fun RecipeDetailsScreen(
@@ -17,8 +19,30 @@ fun RecipeDetailsScreen(
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
+    RecipeDetailsContent(uiState)
+}
+
+@Composable
+fun RecipeDetailsContent(
+    uiState: RecipeDetailsViewModel.UiState
+) {
     Column {
         Image(imageVector = Icons.Default.AccountCircle, contentDescription = "recipe image")
         Text(text = uiState.recipe!!.name)
     }
+}
+
+@Preview
+@Composable
+fun RecipeDetailsContentPreview() {
+    RecipeDetailsContent(
+        uiState = RecipeDetailsViewModel.UiState(
+            recipe = Recipe(
+                id = 0,
+                name = "test",
+                quantity = 1,
+                howToSteps = emptyList()
+            )
+        )
+    )
 }
